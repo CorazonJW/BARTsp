9cf43202db915a152f1d2b90c425cfbf06c7b541
jw4xtu@virginia.edu <jw4xtu@virginia.edu>
2025-06-09 13:46:11 -0400

    data

---
+++ R/data.R
@@ -0,0 +1,16 @@
+#' Mouse Small Intestine Spatial Transcriptomics Dataset
+#'
+#' A spatial transcriptomics dataset from mouse small intestine.
+#'
+#' @format A SummarizedExperiment object containing:
+#' \describe{
+#'   \item{assays}{Gene expression matrix}
+#'   \item{colData}{Sample metadata including spatial coordinates}
+#'   \item{rowData}{Gene annotations}
+#' }
+#'
+#' @source Generated from spatial transcriptomics experiments on mouse small intestine
+#' @examples
+#' data(mm_small_intestine_data)
+#' @name mm_small_intestine_data
+NULL 
