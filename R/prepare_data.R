c785c24f4b42bcb2c2561216662162ac3f99a7d7
jw4xtu@virginia.edu <jw4xtu@virginia.edu>
2025-06-09 14:04:13 -0400

    data

--- R/prepare_data.R
+++
@@ -1,5 +0,0 @@
-# Load the RDS file
-mm_small_intestine_data <- readRDS("data/mm_small_intestine_data.RDS")
-
-# Save the data in the correct format for R package
-usethis::use_data(mm_small_intestine_data, overwrite = TRUE) 
